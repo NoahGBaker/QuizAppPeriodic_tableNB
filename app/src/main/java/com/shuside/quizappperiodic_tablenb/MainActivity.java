c0844946ac003ebe0ca8870537c3e54912b4c145
NoahGBaker <palifonix@gmail.com>
2025-10-27 19:06:00 -0400

    Updates

--- app/src/main/java/com/shuside/quizappperiodic_tablenb/MainActivity.java
+++ app/src/main/java/com/shuside/quizappperiodic_tablenb/MainActivity.java
@@ -2,12 +2,14 @@ package com.shuside.quizappperiodic_tablenb;
 
 import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
+import static android.widget.Toast.LENGTH_SHORT;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,13 +22,16 @@ public class MainActivity extends AppCompatActivity {
     TextView questionNum;
     Button trueButton;
     Button falseButton;
+    Button previous;
 
+    Toast popup;
     ArrayList<Object[]> questions = new ArrayList<Object[]>();
 
     int qnum = 0;
     int numCorrect = 0;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         questionNum = findViewById(R.id.QuestionNumber);
         trueButton = findViewById(R.id.True);
         falseButton = findViewById(R.id.False);
+        previous = findViewById(R.id.previousButton);
+        popup = new Toast(this);
 
         questions.add(new Object[]{"The most abundant element in plastics is Carbon", true});
         questions.add(new Object[]{"The air is made up mostly of Oxygen", false});
@@ -53,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
             Object[] ques = questions.get(qnum);
             if (ques[1].equals(true)) {
                 numCorrect++;
+                Toast.makeText(getApplicationContext(), "Correct!", LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Wrong.", LENGTH_SHORT).show();
             }
 
             if (qnum+1 < questions.size()) {
@@ -67,6 +77,9 @@ public class MainActivity extends AppCompatActivity {
             Object[] quest = questions.get(qnum);
             if (quest[1].equals(false)) {
                 numCorrect++;
+                Toast.makeText(getApplicationContext(), "Correct!", LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Wrong.", LENGTH_SHORT).show();
             }
 
             if (qnum+1 < questions.size()) {
@@ -76,17 +89,19 @@ public class MainActivity extends AppCompatActivity {
                 nextScene();
             }
         });
-
+        previous.setOnClickListener(v -> {
+            if (qnum < questions.size() && qnum != 0) {
+                qnum--;
+                nextQuestion();
+                numCorrect--;
+            }
+        });
     }
 
-    @SuppressLint("SetTextI18n")
+
     public void nextQuestion() {
         Object[] quesi = questions.get(qnum);
         if (qnum < questions.size()) {
-            question.setVisibility(VISIBLE);
-            questionNum.setVisibility(VISIBLE);
-            trueButton.setVisibility(VISIBLE);
-            falseButton.setVisibility(VISIBLE);
             question.setText((String) quesi[0]);
             questionNum.setText("#" + (qnum+1));
         }

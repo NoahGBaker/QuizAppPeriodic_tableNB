3746546276879ae0cd6fc98841191ea2d00f11c7
NoahGBaker <palifonix@gmail.com>
2026-01-14 09:37:02 -0500

    Adding Firebase Basics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 android {
@@ -41,4 +42,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+
+    implementation("com.google.firebase:firebase-analytics")
 }
